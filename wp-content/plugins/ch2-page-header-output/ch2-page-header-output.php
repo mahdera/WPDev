6c12872169dbf0d3f23b9a5af01f8e1ca226c590
Mahder Neway <alemayehu@Mahders-MacBook-Pro.local>
2014-08-14 09:51:32 -0400

    This modification contains addition lines to the two newly added plugins...

---
+++ wp-content/plugins/ch2-page-header-output/ch2-page-header-output.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+    Plugin Name: Chapter 2 – Page Header Output
+    Plugin URI:
+    Description: This is the description line for the second plugin I am playing with...
+    Version: 1.0
+    Author: Mahder Neway
+    Author URI: http://....
+    License: GPLV2       
+    */
+
+?>
+
---
+++ wp-content/plugins/ch2-plugin-header/ch2-plugin-header.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ Plugin Name: Chapter 2 Plugin Header
+ Plugin URI:
+ Description: Declares a plugin that will be visible in the Wordpress admin interface
+ Version: 1.0
+ Author: Mahder Neway
+ Author URI: http://www.mahderjava@blogpost.com
+ License: GPLV2
+ */
+?>
+
